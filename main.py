678cd746931fde600ca1f42f5d39dc4348142167
Camila2025git <camilacubillomora@hotmail.com>
2025-02-04 21:02:39 -0600

    clase 05 actividad sincrónica

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
+# Proyecto: [funciones de estadistica]
+# Estudiante: [Camila]
+# Fecha de Inicio: [04/04/2025]
+# Fecha de Entrega: [04/04/2025]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+from analisis_datos.estadisticas import media,calcular_mediana
+lista_parametro = [3,2,5,4,1]
+print('Media: ', media(lista_paremetro))
